efe1b3ef9b5b7f709e2a744628bda0b16fd8b09f
Tom Slemmons <FEtom@Toms-iMac.local>
2016-09-09 16:54:28 +0900

    update

--- main.py
+++ main.py
@@ -19,29 +19,10 @@ form="""
     <br>
     <br>
 
-    <input type="submit">
+    <code> <input type="submit">
+    </code>
 </form>
 """
-
-
-class MainPage(webapp2.RequestHandler):
-
-
-
-    def get(self):
-        self.response.out.write(form)
-    def post(self):
-        user_month = valid_month(self.request.get('month'))
-        user_month = valid_month(self.request.get('day'))
-        user_month = valid_month(self.request.get('year'))
-
-        if not (user_month and user_day and user_year):
-            self.response.out.write(form)
-        else:
-            self.response.out.write("Thanks!")
-
-app = webapp2.WSGIApplication([('/', MainPage)],
-                                debug=True)
 
 months = ['January',
       'February',
@@ -55,22 +36,42 @@ months = ['January',
       'October',
       'November',
       'December']
-months_abbvs =dict((m[:3].lower(),m) for m in months)
+
+
+months_abbvs = dict((m[:3].lower(),m) for m in months)
+
 
 def valid_month(month):
     if month:
         short_month = month[:3].lower()
-        if cap_month in months:
-    return months_abbvs.get(short_month)
+        return months_abbvs.get(short_month)
 
 def valid_day(day):
-    if day and day = isdigit():
+    if day and day.isdigit():
         day = int(day)
-        if day > 0 and <= 31:
+        if day > 0 and day <= 31:
             return day
 
 def valid_year(year):
-    if year and year = isdigit():
+    if year and year.isdigit():
         year = int(year)
-        if year >1900 and <2020:
+        if year >1900 and year <2020:
             return year
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.out.write(form)
+
+    def post(self):
+        user_month = valid_month(self.request.get('month'))
+        user_day = valid_day(self.request.get('day'))
+        user_year = valid_year(self.request.get('year'))
+
+        if not (user_month and user_day and user_year):
+            self.response.out.write(form)
+        else:
+            self.response.out.write("Thanks!")
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                                debug=True)
